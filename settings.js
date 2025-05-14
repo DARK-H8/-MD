58abccfe478d4176f7e38e71427dc45010987ec4
DARK-H8 <dddds0112520@gmail.com>
2025-05-14 19:26:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'DARK Bot',
   author: '‎',
   botName: "DARK Bot",
-  botOwner: 'DARM', // Your name
+  botOwner: 'DARK', // Your name
   ownerNumber: '112520567', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
